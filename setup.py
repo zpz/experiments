29d4f8045a53a36569e8ef1d32a2526b99d921aa
Zepu Zhang <zepu.zhang@gmail.com>
2018-11-24 23:49:22 -0800

    resume work on this---first, trying to make the code run

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, Extension
 
 extensions = [
     Extension('_cc11binds',
-              sources=['py4cc/_cc11binds.cc'],
+              sources=['src/py4cc/_cc11binds.cc'],
               extra_compile_args=['-std=c++11', '-O3'],
              )
 ]
@@ -13,6 +13,6 @@ setup(
     name='Python/C++ inter-op tester',
     version='0.1.0',
     packages=['py4cc'],
-    ext_package='py4cc',
+    ext_package='src/py4cc',
     ext_modules=extensions,
 )
