6c806d20f4921858757708a81fc87d04d6a2f4ff
Zepu Zhang <zepu.zhang@gmail.com>
2018-12-09 23:30:41 -0800

    rust for py, initial efforts

---
+++ src/rs/datex/src/datex.rs
@@ -0,0 +1,23 @@
+
+pub fn weekday(ts: i64) -> i64 
+{
+    let ts0: i64 = 1489363200;   // 2017-03-13 0:0:0 UTC, Monday
+    let weekday0 = 1;       // ISO weekday: Monday is 1, Sunday is 7
+
+    const DAY_SECONDS: i64 = 86400;
+    const WEEK_SECONDS: i64 = 604800;
+
+    let mut ts_delta = ts - ts0;
+    if ts_delta < 0 {
+        ts_delta += ((-ts_delta) / WEEK_SECONDS + 1) * WEEK_SECONDS;
+    }
+
+    let td: i64 = ts_delta % WEEK_SECONDS;
+    let nday = td / DAY_SECONDS;
+    let weekday: i64 = weekday0 + nday;
+    if weekday > 7 {
+        weekday - 7
+    } else {
+        weekday
+    }
+}
