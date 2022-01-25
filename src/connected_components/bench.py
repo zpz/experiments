36bc16751c267206c6524e35e39defef6b72628a
Zepu Zhang <zepu.zhang@gmail.com>
2022-01-24 19:33:07 -0800

    minor

--- src/connected_components/bench.py
+++ src/connected_components/bench.py
@@ -1,35 +1,40 @@
 import math
-import random
 import time
 import numpy as np
 import cc_nx
+import cc_py
+import cc_np
 import cc_numba
 
 
-def bench(n_items, repeats=1):
-    population = list(range(n_items))
+def bench(mod, n_items, repeats=10):
     for _ in range(repeats):
         n_components = int(math.sqrt(n_items))
         component_size = int(n_items / n_components * 0.8)
         components = [
                 np.random.choice(n_items, component_size, replace=False)
-                for _ in range(n_components)
-                ]
+                for _ in range(n_components)]
 
         t0 = time.perf_counter()
-        _ = cc_numba.connected_components(components, n_items)
+        _ = mod.connected_components(components, n_items)
         t1 = time.perf_counter()
-        tnumba = t1 - t0
+        tpy = t1 - t0
 
         t0 = time.perf_counter()
         _ = cc_nx.connected_components(components)
         t1 = time.perf_counter()
         tnx = t1 - t0
 
-        print(n_items, f'{tnx:.6f}', f'{tnumba:.6f}', f'{tnx/tnumba:.2f}')
+        print(n_items, f'{tnx:.6f}', f'{tpy:.6f}', f'{tnx/tpy:.2f}')
 
 
 if __name__ == '__main__':
-    for n_items in (100, 1000, 10000, 100000, 1000000, 10000000, 100000000):
-        bench(n_items, 4)
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('mod', choices=['py', 'np', 'nu'])
+    args = parser.parse_args()
+
+    mod = {'py': cc_py, 'np': cc_np, 'nu': cc_numba}[args.mod] 
+    for n_items in (100, 1000, 10000, 100000, 1000000):
+        bench(mod, n_items)
 
