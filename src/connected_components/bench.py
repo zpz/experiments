a5a78454ec369beda0f7fd8ba8316aabffb22f15
zpz <zepu.zhang@gmail.com>
2022-02-14 00:35:13 -0800

    m

--- src/connected_components/bench.py
+++ src/connected_components/bench.py
@@ -7,6 +7,9 @@ import cc_numba
 
 
 def bench(mod, n_items, repeats=10):
+    times_ref = []
+    times_test = []
+
     for _ in range(repeats):
         n_components = int(math.sqrt(n_items))
         component_size = int(n_items / n_components * 0.8)
@@ -17,14 +20,18 @@ def bench(mod, n_items, repeats=10):
         t0 = time.perf_counter()
         _ = mod.connected_components(components, n_items)
         t1 = time.perf_counter()
-        tpy = t1 - t0
+        times_test.append(t1 - t0)
 
         t0 = time.perf_counter()
         _ = cc_nx.connected_components(components)
         t1 = time.perf_counter()
-        tnx = t1 - t0
+        times_ref.append(t1 - t0)
 
-        print(n_items, f'{tnx:.6f}', f'{tpy:.6f}', f'{tnx/tpy:.2f}')
+    print('')
+    print('n_items:', n_items, 'n_repeats:', repeats)
+    print('reference vs compare times (min max mean)')
+    print('  ', f'{min(times_ref):.5f}', f'{max(times_ref):.5f}', f'{sum(times_ref)/repeats:.5f}')
+    print('  ', f'{min(times_test):.5f}', f'{max(times_test):.5f}', f'{sum(times_test)/repeats:.5f}')
 
 
 if __name__ == '__main__':
@@ -34,6 +41,10 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     mod = {'py': cc_py, 'nu': cc_numba}[args.mod] 
-    for n_items in (1000, 10000, 100000, 1000000):
+
+    if mod is cc_numba:
+        _ = mod.connected_components([np.array([1,2,3]), np.array([0,2,3,4])], 5) 
+
+    for n_items in (100, 1000, 10000, 100000, 500000):
         bench(mod, n_items)
 
